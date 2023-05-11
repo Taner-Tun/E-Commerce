c61dd650d09df79fa546d7ea5e3c16348d512d4e
Taner Tun <t_tun@msn.com>
2023-05-11 17:20:24 +0300

    Created Card_Items.jsx, FakeData for test and fixed  reusable card components

---
+++ src/routes/FakeData.jsx
@@ -0,0 +1,56 @@
+const cards = [
+    {
+      id:1,
+      title: "Card 1",
+      description: "This is card 1",
+    },
+    {
+        id:2,
+      title: "Card 2",
+      description: "This is card 2",
+    },
+    {
+      id:3,
+      title: "Card 3",
+      description: "This is card 3",
+    },
+    {
+        id:4,
+      title: "Card 4",
+      description: "This is card 4",
+    },
+    {
+        id:5,
+        title: "Card 5",
+        description: "This is card 5",
+      },
+      {
+        id:6,
+        title: "Card 6",
+        description: "This is card 6",
+      },
+      
+      {
+        id:7,
+        title: "Card 7",
+        description: "This is card 7",
+      },
+      {
+        id:8,
+        title: "Card 8",
+        description: "This is card 8",
+      },
+      {
+        id:9,
+        title: "Card 9",
+        description: "This is card 9",
+      },
+      {
+        id:10,
+        title: "Card 10",
+        description: "This is card 10",
+      },
+  ];
+  
+  export default cards;
+  
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,20 @@
 import NavComponent from "./navComponent"
+import Card_Items from "./Card_Items";
+import FakeData from "./FakeData";
 
+function createData (FakeData) {
+  return <Card_Items 
+  key = {FakeData.id}
+  title = {FakeData.title}
+  description = {FakeData.description}
+  />
+}
 function Home() {
+
   return (
     <>
       <h1>Main Page</h1>
+      {FakeData.map(createData)}
       <NavComponent />
     </>
   )
