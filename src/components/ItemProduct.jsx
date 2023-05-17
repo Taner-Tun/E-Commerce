14807753268bdb6c2b8f9d360d0626a768a29dc2
Taner Tun <t_tun@msn.com>
2023-05-17 15:33:53 +0300

    product.jsx updated with add functionality, itemproduct component created, navigate to cart component, styled, swedish text changed toEEnglish

---
+++ src/components/ItemProduct.jsx
@@ -0,0 +1,61 @@
+// import React from "react"
+// import "../scss/main.scss"
+
+// function ItemProduct({itemid, image, title, category, price, count, handleItemClick}) {
+//   const totalPrice = (price * count).toFixed(2)
+
+//   return (
+//     <div className="CartItem" id={itemid}>
+//       <img src={image} alt={title} />
+//       <div className="details">
+//         <h4>{title}</h4>
+//         <h6>{category}</h6>
+//         <h5>
+//           {totalPrice} <p>kr</p>
+//         </h5>
+//         <h2>{price} kr</h2>
+//         <button className="add" onClick={() => handleItemClick("add")}>
+//           Add to Chart
+//         </button>
+//       </div>
+//       <div className="quantity">
+//         <p className="amount">{count}</p>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Item
+import React from "react";
+import "../scss/main.scss";
+
+function ItemProduct({
+  itemid,
+  image,
+  title,
+  category,
+  price,
+  count,
+  handleItemClick,
+}) {
+  const totalPrice = (price * count).toFixed(2);
+
+  return (
+    <div className="CartItem" id={itemid}>
+      <img src={image} alt={title} />
+      <div className="details">
+        <h4>{title}</h4>
+        <h6>{category}</h6>
+        <h2>{price} kr</h2>
+        <button className="add" onClick={() => handleItemClick("add")}>
+          Add to Cart
+        </button>
+      </div>
+      <div className="quantity">
+        <p className="amount">{count}</p>
+      </div>
+    </div>
+  );
+}
+
+export default ItemProduct;
--- src/routes/Products.jsx
+++ src/routes/Products.jsx
@@ -1,9 +1,86 @@
+// import React, { useState, useEffect } from "react";
+// import ItemProduct from "../components/ItemProduct";
+// import "../scss/main.scss";
+
+// function Products() {
+
+//   const [cart, setCart] = useState([]);
+//   const [amount, setAmount] = useState(0);
+//   const [itemCount, setItemCount] = useState(0);
+
+//   useEffect(() => {
+//     fetchData();
+//   }, []);
+
+//   const fetchData = async () => {
+//     try {
+//       const response = await fetch("http://localhost:4000/data");
+//       const data = await response.json();
+//       console.log(data);
+//       setCart(data.map((ItemProduct) => ({ ...ItemProduct, count: 1 })));
+//     } catch (error) {
+//       console.log("Error occurred:", error);
+//     }
+//   };
+
+//   const handleItemClick = (index, action) => {
+//     const updatedCart = [...cart];
+//     const item = updatedCart[index];
+//     switch (action) {
+     
+//       case "add": {
+//         updatedCart.splice(index, 1);
+//         break;
+//       }
+//       default:
+//         break;
+//     }
+//     setCart(updatedCart);
+//   };
+
+//   useEffect(() => {
+//     let totalPrice = 0;
+//     let itemCount = 0;
+//     cart.forEach((item) => {
+//       totalPrice += item.price * item.count;
+//       itemCount += item.count;
+//     });
+//     setAmount(totalPrice.toFixed(2));
+//     setItemCount(itemCount);
+//   }, [cart]);
+
+//   return (
+//     <>
+//       <h1>Products Page</h1>
+//       <div className="test">
+//         <h1 className="Cart">
+//           Shopping Cart <p>{cart.length}</p>
+//         </h1>
+//         <h4>Products</h4>
+//       </div>
+//       {cart.map((ItemProduct, index) => (
+//         <ItemProduct
+//           key={item.itemid}
+//           itemid={item.itemid}
+//           image={item.image}
+//           title={item.title}
+//           category={item.category}
+//           price={item.price}
+//           count={item.count}
+//           handleItemClick={(action) => handleItemClick(index, action)}
+//         />
+//       ))}
+//     </>
+//   )
+// }
+
+// export default Products
+
 import React, { useState, useEffect } from "react";
-import Item from "../components/Item";
+import ItemProduct from "../components/ItemProduct";
 import "../scss/main.scss";
 
 function Products() {
-
   const [cart, setCart] = useState([]);
   const [amount, setAmount] = useState(0);
   const [itemCount, setItemCount] = useState(0);
@@ -17,7 +94,7 @@ function Products() {
       const response = await fetch("http://localhost:4000/data");
       const data = await response.json();
       console.log(data);
-      setCart(data.map((item) => ({ ...item, count: 1 })));
+      setCart(data.map((item) => ({ ...item })));
     } catch (error) {
       console.log("Error occurred:", error);
     }
@@ -27,18 +104,8 @@ function Products() {
     const updatedCart = [...cart];
     const item = updatedCart[index];
     switch (action) {
-      case "+": {
-        item.count += 1;
-        break;
-      }
-      case "-": {
-        if (item.count > 1) {
-          item.count -= 1;
-        }
-        break;
-      }
-      case "delete": {
-        updatedCart.splice(index, 1);
+      case "add": {
+        window.location.href = "/cart";
         break;
       }
       default:
@@ -60,15 +127,15 @@ function Products() {
 
   return (
     <>
-      <h1>Products Page</h1>
+      
       <div className="test">
         <h1 className="Cart">
-          Shopping Cart <p>{cart.length}</p>
+          Products Page <p>{cart.length}</p>
         </h1>
         <h4>Products</h4>
       </div>
       {cart.map((item, index) => (
-        <Item
+        <ItemProduct
           key={item.itemid}
           itemid={item.itemid}
           image={item.image}
@@ -80,7 +147,8 @@ function Products() {
         />
       ))}
     </>
-  )
+  );
 }
 
-export default Products
+export default Products;
+
