2ab039634a0fef6212b5735afa3adb2bcd9d8ea1
Taner Tun <t_tun@msn.com>
2023-05-17 05:16:48 +0300

    fakedata.jsx,cart_items.jsx, removed.home.jsx updated products fetced from backend

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,22 +1,41 @@
-import Card_Items from "../components/Card_Items"
-import FakeData from "../components/FakeData"
-
-function createData(FakeData) {
-  return <Card_Items 
-  key={FakeData.id} 
-  title={FakeData.title} 
-  image={FakeData.image}  
-  category={FakeData.category} 
-  price={FakeData.price} />
-}
+import React, { useState, useEffect } from "react";
+import Item from "../components/Item";
+import "../scss/main.scss";
 
 function Home() {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://localhost:4000/data");
+      const data = await response.json();
+      console.log(data);
+      setCart(data.map((item) => ({ ...item, count: 1 })));
+    } catch (error) {
+      console.log("Error occurred:", error);
+    }
+  };
+
   return (
     <>
       <h1>Main Page</h1>
-      {FakeData.map(createData)}
+      {cart.map((item, index) => (
+        <Item
+          key={item.itemid}
+          itemid={item.itemid}
+          image={item.image}
+          title={item.title}
+          category={item.category}
+          price={item.price}
+          count={item.count}
+        />
+      ))}
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
