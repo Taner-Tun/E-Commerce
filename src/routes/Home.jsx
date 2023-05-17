ca8ce48677e8b3fd736b8a4cb6d7dc0ff19e561f
Taner Tun <t_tun@msn.com>
2023-05-17 07:10:25 +0300

    footer component created, styled, imported to only home.jsx

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Item from "../components/Item";
+import Footer from '../components/Footer';
 import "../scss/main.scss";
 
 function Home() {
   
   return (
-    <>
+    <div className="main-container">
       <h1>Main Page</h1>
-    </>
+      {/* Your main page content... */}
+      <Footer />
+    </div>
   );
 }
 
