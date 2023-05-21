baf44a159667e75a4159c1e1696432e839143223
Taner Tun <t_tun@msn.com>
2023-05-21 15:23:58 +0300

    shopping chart h1 bug fixed, it is counting now

--- src/routes/Cart.jsx
+++ src/routes/Cart.jsx
@@ -1,93 +1,96 @@
-import React, {useState, useEffect} from "react"
-import Item from "../components/Item"
-import Total from "../components/Total"
-import Footer from "../components/Footer"
-import "../scss/main.scss"
+import React, { useState, useEffect } from "react";
+import Item from "../components/Item";
+import Total from "../components/Total";
+import Footer from "../components/Footer";
+import "../scss/main.scss";
 
 function Cart() {
-  const [cart, setCart] = useState([])
-  const [amount, setAmount] = useState(0)
-  const [itemCount, setItemCount] = useState(0)
+  const [cart, setCart] = useState([]);
+  const [amount, setAmount] = useState(0);
+  const [itemCount, setItemCount] = useState(0);
 
   useEffect(() => {
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
   const fetchData = async () => {
     try {
-      const productId = window.location.pathname.split("/").pop()
-      const response = await fetch(`http://localhost:4000/data/${productId}`)
-      const data = await response.json()
-      setCart([{...data, count: 1}])
-    } catch (error) {
-      console.log("Error occurred:", error)
+      const productId = window.location.pathname.split("/").pop();
+      const response = await fetch(`http://localhost:4000/data/${productId}`);
+      const data = await response.json();
+      
+      // Check if the product is already in the cart
+    const existingProductIndex = cart.findIndex((item) => item.id === data.id);
+    if (existingProductIndex !== -1) {
+      // Product already exists, update the count
+      const updatedCart = [...cart];
+      updatedCart[existingProductIndex].count += 1;
+      setCart(updatedCart);
+    } else {
+      // Product doesn't exist, add it to the cart
+      setCart([...cart, { ...data, count: 1 }]);
     }
+  } catch (error) {
+    console.log("Error occurred:", error);
   }
-
+};
   const handleItemClick = (index, action) => {
-    const updatedCart = [...cart]
-    const item = updatedCart[index]
+    const updatedCart = [...cart];
+    const item = updatedCart[index];
     switch (action) {
       case "+": {
-        item.count += 1
-        break
+        item.count += 1;
+        break;
       }
       case "-": {
         if (item.count > 1) {
-          item.count -= 1
+          item.count -= 1;
         }
-        break
+        break;
       }
-      //delete
       case "delete": {
-        updatedCart.splice(index, 1)
-        // Remove cart data from local storage
-        localStorage.removeItem("cart")
-        break
+        updatedCart.splice(index, 1);
+        break;
       }
       default:
-        break
+        break;
     }
 
-    // Store the updated cart data in local storage JSON.stringify store as string
-    localStorage.setItem("cart", JSON.stringify(updatedCart))
-    setCart(updatedCart)
-  }
-
-  //retrive , get data from local storage ), []; will start with first render
-  // JSON.parse convert object
-  // ? means if storedCart is truthy (i.e., not null or undefined).
-  useEffect(() => {
-    const storedCart = localStorage.getItem("cart")
-    const cartData = storedCart ? JSON.parse(storedCart) : []
-    setCart(cartData)
-  }, [])
+    localStorage.setItem("cart", JSON.stringify(updatedCart));
+    setCart(updatedCart);
+  };
 
   useEffect(() => {
-    let totalPrice = 0
-    let itemCount = 0
+    const storedCart = localStorage.getItem("cart");
+    const cartData = storedCart ? JSON.parse(storedCart) : [];
+    setCart(cartData);
+  }, []);
+
+  useEffect(() => {
+    let totalPrice = 0;
+    let itemCount = 0;
     cart.forEach((item) => {
-      totalPrice += item.price * item.count
-      itemCount += item.count
-    })
-    setAmount(totalPrice.toFixed(2))
-    setItemCount(itemCount)
-  }, [cart])
+      totalPrice += item.price * item.count;
+      itemCount += item.count;
+    });
+    setAmount(totalPrice.toFixed(2));
+    setItemCount(itemCount);
+  }, [cart]);
 
   const calculateTotalWithShipping = (totalPrice) => {
-    const totalNum = Number(totalPrice)
+    const totalNum = Number(totalPrice);
     if (totalNum >= 5000) {
-      return totalNum.toFixed(2)
+      return totalNum.toFixed(2);
     } else {
-      return (totalNum + 0).toFixed(2)
+      return totalNum.toFixed(2);
     }
-  }
+  };
 
   return (
     <>
       <div className="test">
         <h1 className="Cart">
-          Shopping Cart <p>{cart.length}</p>
+          Shopping Cart <span>{itemCount}</span>
         </h1>
       </div>
       <div className="main">
@@ -110,7 +113,7 @@ function Cart() {
       </div>
       <Footer />
     </>
-  )
+  );
 }
 
-export default Cart
+export default Cart;
