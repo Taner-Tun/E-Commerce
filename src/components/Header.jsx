deae274516b330eb17ca0160e50447159639874a
Andreas Benckert <andreas.benckert@gmail.com>
2023-05-23 00:01:44 +0200

    Header image and logo updated & favicon added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,15 +9,17 @@ import Navigation from "./Navigation"
 const Header = (props) => {
   return (
     <Fragment>
-      <header className="header-container background">
-        <div className="header-wrapper">
-          <Link className="header-logo-button" to="/" alt="Jensen Deli" title="Jensen Deli" tabIndex={1} aria-label="Jensen Deli">
-            <div className="icon-logo "></div>
-          </Link>
-          <CartButton />
-        </div>
-        <Navigation />
-      </header>
+      <div>
+        <header className="header-container background">
+          <div className="header-wrapper">
+            <Link className="header-logo-button" to="/" alt="Jensen Deli" title="Jensen Deli" tabIndex={1} aria-label="Jensen Deli">
+              <div className="icon-logo "></div>
+            </Link>
+            <CartButton />
+          </div>
+          <Navigation />
+        </header>
+      </div>
     </Fragment>
   )
 }
