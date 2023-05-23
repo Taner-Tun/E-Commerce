b91a0b812bb83c7d9c2dec1dd8d7a1cee7105a5c
Taner Tun <t_tun@msn.com>
2023-05-23 06:43:32 +0300

    filterproductmenu created and styled logic added to console

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,15 +1,19 @@
 import React from "react"
 import Item from "../components/Item"
 import Footer from "../components/Footer"
+import FilterProductMenu from "../components/FilterProductMenu"
 import "../scss/main.scss"
 
 function Home() {
   return (
     <div className="main-container">
+        <FilterProductMenu />
+      <div className="content-box">
       <h1>Main Page</h1>
       {/* Your main page content... */}
       <main></main>
       <Footer />
+      </div>
     </div>
   )
 }
