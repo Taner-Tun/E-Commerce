16b34926584314ebfa7c37f2b88bfaa758a6bee7
Taner Tun <t_tun@msn.com>
2023-05-24 16:46:18 +0300

    filterby is fixed in product.jsx

--- src/components/FilterProductMenu.jsx
+++ src/components/FilterProductMenu.jsx
@@ -1,56 +1,11 @@
 import React, { useState } from "react";
 import "../scss/main.scss";
 
-const FilterProductMenu = () => {
+const FilterProductMenu = ({ handleFilter }) => {
   const [selectedCategory, setSelectedCategory] = useState("");
-
-  const handleFilter = (filterType, category) => {
-    if (filterType === "Category") {
-      // Logic for filtering by category
-      console.log("Filtering by Category");
-
-      // Nest additional logic based on selected category
-      switch (category) {
-        case "Wine":
-          // Logic for filtering Wine
-          console.log("Filtering Wine");
-          break;
-        case "Meat":
-          // Logic for filtering Meat
-          console.log("Filtering Meat");
-          break;
-        case "Cheese":
-          // Logic for filtering Cheese
-          console.log("Filtering Cheese");
-          break;
-        default:
-          // Logic for handling other cases within Category
-          console.log("Handling other cases within Category");
-          break;
-      }
-    } else {
-      switch (filterType) {
-        case "A-Z":
-          // Logic for filtering A-Z
-          console.log("Filtering A-Z");
-          break;
-        case "PriceAsc":
-          // Logic for filtering price from lowest to highest
-          console.log("Filtering Price (Lowest to Highest)");
-          break;
-        case "PriceDesc":
-          // Logic for filtering price from highest to lowest
-          console.log("Filtering Price (Highest to Lowest)");
-          break;
-        default:
-          console.log(`Invalid filter type: ${filterType}`);
-      }
-    }
-  };
 
   const handleCategoryChange = (event) => {
     const category = event.target.value;
-    setSelectedCategory(category);
     handleFilter("Category", category);
   };
 
@@ -85,5 +40,6 @@ const FilterProductMenu = () => {
 };
 
 export default FilterProductMenu;
+
 
 
