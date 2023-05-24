156353018436136e6d2ad097c9b01f97224a366f
Taner Tun <t_tun@msn.com>
2023-05-24 10:14:54 +0300

    quuantity logic added to item product

--- src/components/ItemProduct.jsx
+++ src/components/ItemProduct.jsx
@@ -1,28 +1,82 @@
-import React from "react"
-import "../scss/main.scss"
+// import React from "react"
+// import "../scss/main.scss"
 
-function ItemProduct({itemid, image, title, category, description, price, count, handleItemClick}) {
-  const totalPrice = (price * count).toFixed(2)
+// function ItemProduct({itemid, image, title, category, description, price, count, handleItemClick}) {
+//   const totalPrice = (price * count).toFixed(2)
 
-  const imageLocation = `/src/images/${image}`
+//   const imageLocation = `/src/images/${image}`
+
+//   return (
+//     <div className="CartItem" id={itemid}>
+//       <img src={imageLocation} alt={title} />
+//       <div className="details">
+//         <h4>{title}</h4>
+//         <h6>{category}</h6>
+//         <h4>{description}</h4>
+//         <h2>{price} kr</h2>
+//         <button className="add" onClick={() => handleItemClick("add")}>
+//           Add to Cart
+//         </button>
+//       </div>
+//       <div className="quantity">
+//         <p className="amount">{count}</p>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default ItemProduct
+import React, { useState } from "react";
+import "../scss/main.scss";
+
+function ItemProduct({ itemid, image, title, category, description, price, count, handleItemClick }) {
+  const [buttonView, setButtonView] = useState("add"); // State variable for button view
+  const [quantity, setQuantity] = useState(count); // State variable for quantity
+  const totalPrice = (price * quantity).toFixed(2);
+
+  const handleButtonClick = () => {
+    if (buttonView === "add") {
+      setButtonView("quantity");
+      handleItemClick("add");
+    }
+  };
+
+  const handleIncrement = () => {
+    setQuantity(prevQuantity => prevQuantity + 1);
+  };
+
+  const handleDecrement = () => {
+    if (quantity > 1) {
+      setQuantity(prevQuantity => prevQuantity - 1);
+    }
+  };
 
   return (
     <div className="CartItem" id={itemid}>
-      <img src={imageLocation} alt={title} />
+      <img src={image} alt={title} />
       <div className="details">
         <h4>{title}</h4>
         <h6>{category}</h6>
         <h4>{description}</h4>
         <h2>{price} kr</h2>
-        <button className="add" onClick={() => handleItemClick("add")}>
-          Add to Cart
-        </button>
-      </div>
-      <div className="quantity">
-        <p className="amount">{count}</p>
+        {buttonView === "add" ? (
+          <button className="add" onClick={handleButtonClick}>
+            Add to Cart
+          </button>
+        ) : (
+          <div className="quantity">
+            <button className="decrement" onClick={handleDecrement}>
+              -
+            </button>
+            <p className="amount">{quantity}</p>
+            <button className="increment" onClick={handleIncrement}>
+              +
+            </button>
+          </div>
+        )}
       </div>
     </div>
-  )
+  );
 }
 
-export default ItemProduct
+export default ItemProduct;
