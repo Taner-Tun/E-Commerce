946ab25bdf2733776819773de35749c8162930c9
Andreas Benckert <andreas.benckert@gmail.com>
2023-05-25 11:04:22 +0200

    Error update

--- src/routes/Error.jsx
+++ src/routes/Error.jsx
@@ -1,12 +1,19 @@
 // import MainNav from "../components/MainNav"
 
+import {Link} from "react-router-dom"
+
 function ErrorPage() {
   return (
     <>
       {/* <MainNav /> */}
       <main>
         <h1>An error has occured!</h1>
-        <p>Could not find this page.</p>
+        <p>
+          Could not find this page. Click on the following link to go back to the{" "}
+          <Link to="/" style={{color: "red"}}>
+            start page
+          </Link>
+        </p>
       </main>
     </>
   )
