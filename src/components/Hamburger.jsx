4b3baed437b16bddd43f1e4e332b913e9bf2805d
Andreas Benckert <andreas.benckert@gmail.com>
2023-06-03 13:38:23 +0200

    Focus working

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -30,23 +30,23 @@ function Hamburger() {
   }, [])
   return (
     <Fragment>
-      <button className="hamburger-toggle" aria-controls="hamburger-wrapper" aria-expanded="false">
+      <button className="hamburger-toggle" aria-controls="hamburger-wrapper" aria-expanded="false" tabIndex={1}>
         <span className="sr-only">Menu</span>
       </button>
       <nav id="hamburger-wrapper" className="hamburger-wrapper" data-visible="true">
         <ul>
           <li>
-            <Link to="/" tabIndex={1} aria-label="Products" title="Products">
+            <Link to="/" tabIndex={2} aria-label="Products" title="Products">
               Products
             </Link>
           </li>
           <li>
-            <Link to="/shoppingcart" tabIndex={2} aria-label="Shopping Cart" title="Shopping Cart">
+            <Link to="/shoppingcart" tabIndex={3} aria-label="Shopping Cart" title="Shopping Cart">
               Shopping Cart
             </Link>
           </li>
           <li>
-            <Link to="/contact" tabIndex={3} aria-label="Contact Us" title="Contact Us">
+            <Link to="/contact" tabIndex={4} aria-label="Contact Us" title="Contact Us">
               Contact Us
             </Link>
           </li>
