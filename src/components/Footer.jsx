a6aa1e7e2b4e24d1dd13d1b307cdf112a08f6e55
Taner Tun <t_tun@msn.com>
2023-06-03 12:37:07 +0300

    opening hours details moved from cart component to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,6 +6,15 @@ function Footer() {
 
   return (
     <footer className="footer">
+       <div className="contact-section">
+        <div className="call">
+          <h1>Call Us</h1>
+          <h3>Opening Hours</h3>
+          <p>Monday–friday: 9-18</p>
+          <p>Saturday–Sunday: Closed</p>
+          <button className="btn1">078 768 500</button>
+        </div>
+      </div>
             <div className="social-media">
     <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-f"></i></a>
     <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter"></i></a>
